b1b22206e5d095dfef29fae19b17306e7d04f725
RobloxAvatar <85132986+RobloxAvatar@users.noreply.github.com>
2022-06-20 21:48:14 +0200

    Create Main.lua

---
+++ Main.lua
@@ -0,0 +1,12 @@
+local library = loadstring(game:HttpGet(('https://raw.githubusercontent.com/RobloxAvatar/uiLibary/main/Uilib.lua')))()
+
+local w = library:CreateWindow("JailBreak")
+local MainTab = w:CreateFolder("Main")
+
+local engine = loadstring(game:HttpGet("https://raw.githubusercontent.com/RobloxAvatar/JailWare/main/Engine.lua"))()
+
+MainTab:Button("Open All Doors", function()
+    for i,v in next, engine.Doors do 
+        engine.OpenDoor(v)
+    end
+end)
