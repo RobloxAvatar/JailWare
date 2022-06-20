f52a3b997a0086ca088113df9a7ce073e25ed005
RobloxAvatar <85132986+RobloxAvatar@users.noreply.github.com>
2022-06-20 21:34:22 +0200

    Create Engine.lua

---
+++ Engine.lua
@@ -0,0 +1,17 @@
+local JailWare = {}
+JailWare.Doors = {}
+
+for i, v in pairs(getgc(true)) do
+    if type(v) == "table" then
+        if rawget(v, "State") and rawget(v, "OpenFun") then
+            table.insert(JailWare.Doors, v)
+        end
+    elseif type(v) == "function" then
+        if getfenv(v).script == game:GetService("Players").LocalPlayer.PlayerScripts.LocalScript then
+            local con = getconstants(v)
+            if table.find(con, "SequenceRequireState") then
+                JailWare.OpenDoor = v
+            end
+        end
+    end
+end
