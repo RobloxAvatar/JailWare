5f6dccf59c56cdba110fa08ce66ea58a045ae0c3
RobloxAvatar <85132986+RobloxAvatar@users.noreply.github.com>
2022-06-21 14:47:10 +0200

    Update Engine.lua

--- Engine.lua
+++ Engine.lua
@@ -16,4 +16,9 @@ for i, v in pairs(getgc(true)) do
     end
 end
 
+function JailWare.MakeNotification(txt, time)
+    local p = loadstring(game:HttpGet("https://raw.githubusercontent.com/RobloxAvatar/JailWare/main/Notify.lua"))()
+	p:MakeNotification({Name = "JailWare", Content = txt, Time = time})
+end
+
 return JailWare
