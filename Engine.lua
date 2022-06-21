c4d9fd4ba6610f27587f995181804ccaef02ae0a
RobloxAvatar <85132986+RobloxAvatar@users.noreply.github.com>
2022-06-21 14:56:08 +0200

    Update Engine.lua

--- Engine.lua
+++ Engine.lua
@@ -1,24 +1,26 @@
 local JailWare = {}
 JailWare.Doors = {}
 
-for i, v in pairs(getgc(true)) do
-    if type(v) == "table" then
-        if rawget(v, "State") and rawget(v, "OpenFun") then
-            table.insert(JailWare.Doors, v)
-        end
-    elseif type(v) == "function" then
-        if getfenv(v).script == game:GetService("Players").LocalPlayer.PlayerScripts:FindFirstChild("LocalScript") then
-            local con = getconstants(v)
-            if table.find(con, "SequenceRequireState") then
-                JailWare.OpenDoor = v
-            end
-        end
-    end
-end
-
 function JailWare.MakeNotification(txt, time)
     local p = loadstring(game:HttpGet("https://raw.githubusercontent.com/RobloxAvatar/JailWare/main/Notify.lua"))()
 	p:MakeNotification({Name = "JailWare", Content = txt, Time = time})
+end
+
+if game.PlaceId == 606849621 then
+    for i, v in pairs(getgc(true)) do
+        if type(v) == "table" then
+            if rawget(v, "State") and rawget(v, "OpenFun") then
+                table.insert(JailWare.Doors, v)
+            end
+        elseif type(v) == "function" then
+            if getfenv(v).script == game:GetService("Players").LocalPlayer.PlayerScripts:FindFirstChild("LocalScript") then
+                local con = getconstants(v)
+                if table.find(con, "SequenceRequireState") then
+                    JailWare.OpenDoor = v
+                end
+            end
+        end
+    end
 end
 
 return JailWare
